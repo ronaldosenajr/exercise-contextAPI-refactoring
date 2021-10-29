209e32645705f4d35ff03addf6f8136c1fe40d81
ronaldosenajr <ronaldosenajr@gmail.com>
2021-10-29 17:45:53 -0400

    Refatorado as funcoes

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,59 +1,55 @@
 // src/Cars.jsx
 
-import React from 'react';
+import React, { useContext } from 'react';
 import CarContext from './context/CarContext';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
 
 function Cars() {
+  const { cars: { red, blue, yellow }, moveCar } = useContext(CarContext);
   return (
-    <CarContext.Consumer>
-      {
-        ({ cars: { red, blue, yellow }, moveCar }) =>
-          <div>
-            <div>
-              <img
-                className={red ? 'car-right' : 'car-left'}
-                src={carRed}
-                alt="red car"
-              />
-              <button
-                onClick={() => moveCar('red', !red)}
-                type="button"
-              >
-                Move
-              </button>
-            </div>
-            <div>
-              <img
-                className={blue ? 'car-right' : 'car-left'}
-                src={carBlue}
-                alt="blue car"
-              />
-              <button
-                onClick={() => moveCar('blue', !blue)}
-                type="button"
-              >
-                Move
-              </button>
-            </div>
-            <div>
-              <img
-                className={yellow ? 'car-right' : 'car-left'}
-                src={carYellow}
-                alt="yellow car"
-              />
-              <button
-                onClick={() => moveCar('yellow', !yellow)}
-                type="button"
-              >
-                Move
-              </button>
-            </div>
-          </div>
-      }
-    </CarContext.Consumer>
+    <div>
+      <div>
+        <img
+          className={red ? 'car-right' : 'car-left'}
+          src={carRed}
+          alt="red car"
+        />
+        <button
+          onClick={() => moveCar('red', !red)}
+          type="button"
+        >
+          Move
+        </button>
+      </div>
+      <div>
+        <img
+          className={blue ? 'car-right' : 'car-left'}
+          src={carBlue}
+          alt="blue car"
+        />
+        <button
+          onClick={() => moveCar('blue', !blue)}
+          type="button"
+        >
+          Move
+        </button>
+      </div>
+      <div>
+        <img
+          className={yellow ? 'car-right' : 'car-left'}
+          src={carYellow}
+          alt="yellow car"
+        />
+        <button
+          onClick={() => moveCar('yellow', !yellow)}
+          type="button"
+        >
+          Move
+        </button>
+      </div>
+    </div>
   );
 }
 
--- src/TrafficSignal.jsx
+++ src/TrafficSignal.jsx
@@ -1,39 +1,36 @@
 // src/TrafficSignal.jsx
 
-import React from 'react';
+import React, { useContext } from 'react';
 import TrafficContext from './context/TrafficContext'
 import redSignal from './images/redSignal.jpeg';
 import yellowSignal from './images/yellowSignal.jpeg';
 import greenSignal from './images/greenSignal.jpeg';
 
 const renderSignal = (signalColor) => {
-  if (signalColor === 'red') return redSignal;
-  if (signalColor === 'yellow') return yellowSignal;
-  if (signalColor === 'green') return greenSignal;
-  return null;
+	if (signalColor === 'red') return redSignal;
+	if (signalColor === 'yellow') return yellowSignal;
+	if (signalColor === 'green') return greenSignal;
+	return null;
 };
 
 const TrafficSignal = () => {
-  return (
-    <TrafficContext.Consumer>
-      {({changeSignal, signalColor: {signal: {color}}}) =>
-        <div>
-          <div className="button-container">
-            <button onClick={() => changeSignal('red')} type="button">
-              Red
-            </button>
-            <button onClick={() => changeSignal('yellow')} type="button">
-              Yellow
-            </button>
-            <button onClick={() => changeSignal('green')} type="button">
-              Green
-            </button>
-          </div>
-          <img className="signal" src={renderSignal(color)} alt="" />
-        </div>
-      }
-    </TrafficContext.Consumer>
-  );
+	const { changeSignal, signalColor: { signal: { color } } } = useContext(TrafficContext)
+	return (
+		<div>
+			<div className="button-container">
+				<button onClick={() => changeSignal('red')} type="button">
+					Red
+				</button>
+				<button onClick={() => changeSignal('yellow')} type="button">
+					Yellow
+				</button>
+				<button onClick={() => changeSignal('green')} type="button">
+					Green
+				</button>
+			</div>
+			<img className="signal" src={renderSignal(color)} alt="" />
+		</div>
+	);
 };
 
 
